153d1accc25ce5f8842a9b40b482b731a539c14e
Andre-Krasnievicz <andrekrascosta@gmail.com>
2025-07-19 22:31:30 -0400

    adjust: corrigido o Page_2.py -> page_2.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 main_page = st.Page('main_page.py', title='Diagrama Skewt', icon='🌡️')
-page_2 = st.Page('Page_2.py', title='Meteograma', icon='📈')
+page_2 = st.Page('page_2.py', title='Meteograma', icon='📈')
 
 pg = st.navigation([main_page, page_2])
 
